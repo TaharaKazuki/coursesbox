b7847784377f36ca4971a6356a2fcd8518ef752e
Tahara Kazuki <ssp0727.lnc@gmail.com>
2022-11-04 11:02:18 +0900

    update

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,15 +1,28 @@
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin')
+
 module.exports = {
   stories: [
     '../pages/**/*.stories.mdx',
     '../pages/**/*.stories.@(js|jsx|ts|tsx)',
     '../components/**/*.stories.mdx',
     '../components/**/*.stories.@(js|jsx|ts|tsx)',
+    '../stories/**/*.stories.mdx',
+    '../stories/**/*.stories.@(js|jsx|ts|tsx)',
   ],
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
     '@storybook/addon-interactions',
   ],
+  webpackFinal: async (config) => {
+    config.resolve.plugins = [
+      ...(config.resolve.plugins || []),
+      new TsconfigPathsPlugin({
+        extensions: config.resolve.extensions,
+      }),
+    ]
+    return config
+  },
   framework: '@storybook/react',
   core: {
     builder: '@storybook/builder-webpack5',
