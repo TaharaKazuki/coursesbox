9dfd73ff53688cef961b3cb52ca4209519528dd1
Tahara Kazuki <ssp0727.lnc@gmail.com>
2022-11-08 09:24:31 +0900

    update

--- components/Checkbox/Checkbox.tsx
+++ components/Checkbox/Checkbox.tsx
@@ -4,11 +4,11 @@ import styled from '@emotion/styled'
 import { boxShadow, transition } from '@/components/styles'
 import { AppTheme } from '@/styles/themes'
 
-type WrapperProps = {
+type StyledProps = {
   theme?: AppTheme
 }
 
-const Wrapper = styled.label<WrapperProps>`
+const Wrapper = styled.label<StyledProps>`
   font-size: 1.8rem;
   & input {
     display: none;
@@ -18,6 +18,23 @@ const Wrapper = styled.label<WrapperProps>`
       ${({ theme }) => boxShadow(theme.components.shadow1, theme.components.shadow2, true)}
       color: ${({ theme }) => theme.font.regular}
     }
+  }
+`
+
+const VisiblePart = styled.label<StyledProps>`
+  display: inline-block;
+  user-select: none;
+  cursor: pointer;
+  text-align: center;
+  border-radius: 0.5rem;
+  width: 2.5rem;
+  height: 2.5rem;
+  color: rgba(0, 0, 0, 0);
+  background: ${({ theme }) => theme.components.background};
+  ${({ theme }) => boxShadow(theme.components.shadow1, theme.components.shadow2)}
+  ${transition()};
+  &:hover {
+    ${({ theme }) => boxShadow(theme.components.shadow1, theme.components.shadow2, true)}
   }
 `
 
